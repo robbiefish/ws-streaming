1b09ea562d816fac87c93db71087a1407759778a
Viacheslau Kalenikau <viacheslau.kalenikau@gmail.com>
2026-08-05 15:09:52 +0200

    Parse the receive buffer in place instead of memmoving after every frame

--- src/detail/peer.cpp
+++ src/detail/peer.cpp
@@ -204,24 +204,34 @@ void wss::detail::peer::process_buffer()
 
 void wss::detail::peer::process_buffer_tcp()
 {
+    std::size_t offset = 0;
+
     // Process as many streaming protocol packets as possible.
     while (true)
     {
         std::size_t bytes_consumed = process_packet(
-            _rx_buffer.data(),
-            _rx_buffer_bytes);
+            _rx_buffer.data() + offset,
+            _rx_buffer_bytes - offset);
 
         // If there's not enough data to form a complete packet, we can't process any more.
         if (!bytes_consumed)
             break;
 
-        // Consume the handled frame by sliding the remaining data in the read buffer over
-        // to the left. (Can't use std::memcpy() for this because the ranges overlap.)
+        // Consume the handled packet by leaving it behind: nothing is moved while the loop runs.
+        offset += bytes_consumed;
+    }
+
+    // The loop only stops once what remains is an incomplete packet, so at most one packet is
+    // left. Slide it to the front so that the next read has the rest of the buffer to fill.
+    // (Can't use std::memcpy() for this because the ranges overlap.)
+    if (offset)
+    {
+        _rx_buffer_bytes -= offset;
+
         std::memmove(
-            &_rx_buffer[0],
-            &_rx_buffer[bytes_consumed],
-            _rx_buffer_bytes - bytes_consumed);
-        _rx_buffer_bytes -= bytes_consumed;
+            _rx_buffer.data(),
+            _rx_buffer.data() + offset,
+            _rx_buffer_bytes);
     }
 
     // If the read buffer is still full after processing, it is an error condition
@@ -234,13 +244,15 @@ void wss::detail::peer::process_buffer_tcp()
 
 void wss::detail::peer::process_buffer_ws()
 {
+    std::size_t offset = 0;
+
     // Process as many WebSocket frames as possible.
     while (true)
     {
         // Try to decode the WebSocket header.
         auto header = detail::websocket_protocol::decode_header(
-            _rx_buffer.data(),
-            _rx_buffer_bytes);
+            _rx_buffer.data() + offset,
+            _rx_buffer_bytes - offset);
 
         // If there's not enough data to form a complete frame, we can't process any more.
         if (!header.header_size)
@@ -249,20 +261,28 @@ void wss::detail::peer::process_buffer_ws()
         boost::system::error_code process_ec;
         process_websocket_frame(
             header,
-            _rx_buffer.data() + header.header_size,
+            _rx_buffer.data() + offset + header.header_size,
             header.payload_size,
             process_ec);
 
         if (process_ec)
             return close(process_ec);
 
-        // Consume the handled frame by sliding the remaining data in the read buffer over
-        // to the left. (Can't use std::memcpy() for this because the ranges overlap.)
+        // Consume the handled frame by leaving it behind: nothing is moved while the loop runs.
+        offset += header.header_size + header.payload_size;
+    }
+
+    // The loop only stops once what remains is an incomplete frame, so at most one frame is left.
+    // Slide it to the front so that the next read has the rest of the buffer to fill.
+    // (Can't use std::memcpy() for this because the ranges overlap.)
+    if (offset)
+    {
+        _rx_buffer_bytes -= offset;
+
         std::memmove(
-            &_rx_buffer[0],
-            &_rx_buffer[header.header_size + header.payload_size],
-            _rx_buffer_bytes - (header.header_size + header.payload_size));
-        _rx_buffer_bytes -= header.header_size + header.payload_size;
+            _rx_buffer.data(),
+            _rx_buffer.data() + offset,
+            _rx_buffer_bytes);
     }
 
     // If the read buffer is still full after processing, it is an error condition
